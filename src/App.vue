c1a934ce0e65734040e26178ee64dfae0e44a843
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-19 15:45:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  import HeaderNav from './components/HeaderNav.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderNav,
+    },
+    data () {
+      return {
+        //
+      }
+    },
+    methods: {
+      // 
+    },
+    computed: {
+      // 
+    },
+  }
+</script>
+
+<template>
+  <header>
+    <HeaderNav />
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,40 @@
+<!-- Header Nav  -->
+<script>
+export default {
+  name: 'HeaderNav',
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+    // 
+  },
+  computed: {
+    // 
+  },
+}
+</script>
+
+<!-- template -->
+<template>
+  <div class="header-nav">
+    <div class="header-nav__logo">
+      <img src="../assets/logo.png" alt="logo">
+    </div>
+    <div class="header-nav__menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<!-- style CSS -->
+<style scoped>
+
+</style>
