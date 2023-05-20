f7d4e67cafaf41684a5aa3bcc1ab0ebd21c15bfc
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-20 15:19:58 +0200

    fix an add App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
   import HeaderNav from './components/HeaderNav.vue'
   import MainContent from './components/MainContent.vue'
+  import ShopComics from './components/ShopComics.vue'
+  import FooterNav from './components/FooterNav.vue'
+  import FooterBottom from './components/FooterBottom.vue'
 
   export default {
     name: 'App',
     components: {
       HeaderNav,
       MainContent,
+      ShopComics,
+      FooterNav,
+      FooterBottom,
     },
     data () {
       return {
@@ -24,16 +30,30 @@
 
 <!-- template -->
 <template>
+
   <!-- header -->
   <header>
     <!-- header nav -->
     <HeaderNav />
   </header>
+
   <!-- main -->
   <main>
     <!-- main content -->
     <MainContent />
+    <!-- Shop content -->
+    <ShopComics />
   </main>
+
+  <!-- footer -->
+  <footer>
+    <!-- footer nav -->
+    <FooterNav />
+
+    <!-- footer bottom -->
+    <FooterBottom />
+  </footer>
+
 </template>
 
 <style scoped></style>
