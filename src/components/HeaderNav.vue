54b23bc45899262d35d7df4e660a75288482a821
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-20 15:20:15 +0200

    Add HeaderNav

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,39 +1,81 @@
 <!-- Header Nav  -->
 <script>
-export default {
-  name: 'HeaderNav',
-  data () {
-    return {
+  export default {
+    name: 'HeaderNav',
+    data () {
+      return {
+
+      imageLogo : [
+        {
+          image: './public/vue-dc-comics-1/img/dc-logo.png',
+          alt: 'logo',
+        }
+      ],
+
+      menu: [
+        {
+          name: 'Characters',
+          link: '#'
+        },
+        {
+          name: 'Comics',
+          link: '#'
+        },
+        {
+          name: 'Movies',
+          link: '#'
+        },
+        {
+          name: 'Tv',
+          link: '#'
+        },
+        {
+          name: 'Games',
+          link: '#'
+        },
+        {
+          name: 'Collectibles',
+          link: '#'
+        },
+        {
+          name: 'Videos',
+          link: '#'
+        },
+        {
+          name: 'Fans',
+          link: '#'
+        },
+        {
+          name: 'News',
+          link: '#'
+        },
+        {
+          name: 'Shop',
+          link: '#'
+        },
+      ]
+      }
+    },
+    methods: {
+      //
+    },
+    computed: {
       // 
-    }
-  },
-  methods: {
-    // 
-  },
-  computed: {
-    // 
-  },
-}
+    },
+  }
 </script>
 
 <!-- template -->
 <template>
   <div class="header-nav">
     <div class="header-nav-logo">
-      <img src="//public/vue-dc-comics-1/img/dc-logo.png" alt="logo">
+      <img :src="imageLogo[0].image" :alt="imageLogo[0].alt">
     </div>
     <div class="header-nav-menu">
       <ul>
-        <li><a href="#"></a>Characters</li>
-        <li><a href="#">Comics</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">Tv</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Collectibles</a></li>
-        <li><a href="#">Videos</a></li>
-        <li><a href="#">Fans</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Shop</a></li>
+        <li v-for="(item, index) in menu" :key="index">
+          <a :href="item.link">{{ item.name }}</a>
+        </li>
       </ul>
     </div>
   </div>
@@ -45,13 +87,16 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 20px 0;
+  padding: 2rem 0;
   margin: 0 auto;
   max-width: 1200px;
 }
 
 .header-nav-logo img {
-  width: 100px;
+  width: 6.25rem;
+  object-fit: cover;
+  object-position: center;
+  cursor: pointer;
 }
 
 .header-nav-menu ul {
